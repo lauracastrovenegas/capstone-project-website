eb1ea5ade08ff2e7c7994d70d06095b946144afd
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-03-29 17:13:27 -0400

    created nav component and page section structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AssistiveTech from './AssistiveTech';
 import LearnASL from './LearnASL';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
+      <header>
+        <Navbar/>
+      </header>
+      <main>
+        <Routes>
         <Route exact path="/" element={<AssistiveTech />} />
         <Route exact path="/asl" element={<LearnASL />} />
       </Routes>
+      </main>
     </BrowserRouter>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav>
+            nav here
+        </nav>
+    )
+}
+
+export default Navbar;
