f0a673561b379711067b3e2206de72de329e2fdb
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 12:06:57 -0400

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const ALDsSectionData = [
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Hearing loop (or induction loop) systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "FM Systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Infrared Systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Personal amplifiers",
+        videoLink: "",
+    }
+]
+
+export { ALDsSectionData };
