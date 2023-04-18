5e2aa27556250474ea25afbb26fc48aad8464ff6
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 13:38:15 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -26,30 +26,58 @@ const ALDsSectionData = [
 ]
 
 const AACSectionData = [
-    {
-        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
-        imgAlt: "",
-        text: "Hearing loop (or induction loop) systems",
-        videoLink: "",
-    },
-    {
-        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
-        imgAlt: "",
-        text: "FM Systems",
-        videoLink: "",
-    },
-    {
-        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
-        imgAlt: "",
-        text: "Infrared Systems",
-        videoLink: "",
-    },
-    {
-        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
-        imgAlt: "",
-        text: "Personal amplifiers",
-        videoLink: "",
-    }
+    [
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Hearing loop (or induction loop) systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "FM Systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Infrared Systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Personal amplifiers",
+            videoLink: "",
+        }
+    ],
+    [
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Hearing loop (or induction loop) systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "FM Systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Infrared Systems",
+            videoLink: "",
+        },
+        {
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+            imgAlt: "",
+            text: "Personal amplifiers",
+            videoLink: "",
+        }
+    ]
 ]
 
 export { ALDsSectionData };
