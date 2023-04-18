443a583280a5b07da5cb045329a303c0a02bff87
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 14:21:58 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -80,5 +80,39 @@ const AACSectionData = [
     ]
 ]
 
+const ADSectionData = [
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Hearing loop (or induction loop) systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "FM Systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Infrared Systems",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Personal amplifiers",
+        videoLink: "",
+    },
+    {
+        imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
+        imgAlt: "",
+        text: "Personal amplifiers",
+        videoLink: "",
+    }
+]
+
 export { ALDsSectionData };
 export { AACSectionData };
+export { ADSectionData };
