decf35067852baf765b9c9974efadf6f1cc9f431
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 14:22:34 -0400

    Create Popup.js

---
+++ src/components/Popup.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+const PopupWrapper = styled.div`
+
+`
+
+const Popup = () => {
+    return (
+        <PopupWrapper>
+
+        </PopupWrapper>
+    )
+}
+
+export default Popup;
