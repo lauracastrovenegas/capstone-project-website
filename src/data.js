738d37e1865848c4abb5c41c920ff63c12444e1f
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 16:03:14 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,27 +1,42 @@
+const AssistiveTechData = [
+    {
+        title: "Assistive listening devices (ALDs)",
+        text: "Assistive listening devices (ALDs) help amplify the sounds you want to hear, especially where there’s a lot of background noise. ALDs can be used with a hearing aid or cochlear implant to help a wearer hear certain sounds better.",
+    },
+    {
+        title: "Augmentative and alternative communication (AAC) devices",
+        text: "Augmentative and alternative communication (AAC) devices help people with communication disorders to express themselves. These devices can range from a simple picture board to a computer program that synthesizes speech from text.",
+    },
+    {
+        title: "Alerting devices",
+        text: "Alerting devices connect to a doorbell, telephone, or alarm that emits a loud sound or blinking light to let someone with hearing loss know that an event is taking place.",
+    },
+]
+
 const ALDsSectionData = [
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
         title: "Hearing loop systems",
-        videoLink: "",
+        videoLink: "https://www.youtube.com/embed/ifBKOomw8SY?start=43",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
         title: "FM Systems",
-        videoLink: "",
+        videoLink: "https://www.youtube.com/embed/JNzxOJKCUug",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
         title: "Infrared Systems",
-        videoLink: "",
+        videoLink: "https://www.youtube.com/embed/LM03-EC73To?start=389",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
         title: "Personal amplifiers",
-        videoLink: "",
+        videoLink: "https://www.youtube.com/embed/ifBKOomw8SY?start=43",
     }
 ]
 
@@ -111,21 +126,6 @@ const ADSectionData = [
         title: "Personal amplifiers",
         videoLink: "",
     }
-]
-
-const AssistiveTechData = [
-    {
-        title: "Assistive listening devices (ALDs)",
-        text: "Assistive listening devices (ALDs) help amplify the sounds you want to hear, especially where there’s a lot of background noise. ALDs can be used with a hearing aid or cochlear implant to help a wearer hear certain sounds better.",
-    },
-    {
-        title: "Augmentative and alternative communication (AAC) devices",
-        text: "",
-    },
-    {
-        title: "Alerting devices",
-        text: "",
-    },
 ]
 
 export { ALDsSectionData };
