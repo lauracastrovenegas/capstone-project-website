e7c85263b9e2937f38d8af18f31557273142ee30
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 15:52:06 -0400

    Update SquareCard.js

--- src/components/SquareCard.js
+++ src/components/SquareCard.js
@@ -1,25 +1,4 @@
 import styled from "styled-components"
-import theme from "../theme"
-
-const Wrapper = styled.div`
-    display: flex;
-    background-color: white;
-    border-radius: 15px;
-    padding: 3rem 1.5rem;
-    flex: 1;
-    margin: 2rem 1rem;
-    box-shadow: 0px 67.11px 109.06px rgba(0, 0, 0, 0.05);
-    border: 1px solid rgba(216, 216, 216, 0.4);
-
-    transition: transform 1s;
-    
-    :hover{
-        transform: scale(1.05);
-        cursor: pointer;
-        color: white;
-        background-color: ${theme.colors.purple};
-    }
-`
 
 const Text = styled.div`
     margin: auto auto;
@@ -31,9 +10,7 @@ const Text = styled.div`
 
 const SquareCard = ({children}) => {
     return (
-        <Wrapper>
-            <Text>{children}</Text>
-        </Wrapper>
+        <Text>{children}</Text>
     )
 }
 
