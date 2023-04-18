ed012f7fd5c09b71b46c3bee9241e60b3d0b8c6c
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 15:52:39 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,25 +2,25 @@ const ALDsSectionData = [
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Hearing loop (or induction loop) systems",
+        title: "Hearing loop systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "FM Systems",
+        title: "FM Systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Infrared Systems",
+        title: "Infrared Systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Personal amplifiers",
+        title: "Personal amplifiers",
         videoLink: "",
     }
 ]
@@ -84,35 +84,51 @@ const ADSectionData = [
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Hearing loop (or induction loop) systems",
+        title: "Hearing loop (or induction loop) systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "FM Systems",
+        title: "FM Systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Infrared Systems",
+        title: "Infrared Systems",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Personal amplifiers",
+        title: "Personal amplifiers",
         videoLink: "",
     },
     {
         imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/VTUZB27OH5HXJP64JHWC7QGHLA.jpg",
         imgAlt: "",
-        text: "Personal amplifiers",
+        title: "Personal amplifiers",
         videoLink: "",
     }
+]
+
+const AssistiveTechData = [
+    {
+        title: "Assistive listening devices (ALDs)",
+        text: "Assistive listening devices (ALDs) help amplify the sounds you want to hear, especially where there’s a lot of background noise. ALDs can be used with a hearing aid or cochlear implant to help a wearer hear certain sounds better.",
+    },
+    {
+        title: "Augmentative and alternative communication (AAC) devices",
+        text: "",
+    },
+    {
+        title: "Alerting devices",
+        text: "",
+    },
 ]
 
 export { ALDsSectionData };
 export { AACSectionData };
 export { ADSectionData };
+export { AssistiveTechData };
