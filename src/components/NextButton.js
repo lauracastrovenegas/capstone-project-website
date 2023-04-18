de3f18998d0514443cfbb6f8671e9a4c313e2e44
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2023-04-18 14:22:23 -0400

    Create NextButton.js

---
+++ src/components/NextButton.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import theme from "../theme";
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+const Wrapper = styled.div`
+    display: flex;
+`
+
+const ButtonWrapper = styled.button`
+    color: ${theme.colors.purple};
+    background-color: ${theme.colors.white};
+    border-color: ${theme.colors.purple};
+    border-style: solid;
+    border-width: 5px;
+    border-radius: 30px;
+    padding: 1.5rem 2rem;
+    width: 23rem;
+    cursor: pointer;
+
+    :hover {
+        background-color: ${theme.colors.purple};
+        color: white;
+    }
+`
+
+const Text = styled.div`
+    font-size: 2rem;
+    font-weight: 800;
+    width: fit-content;
+    display: flex;
+    margin: 0rem auto;
+
+    svg {
+        margin: auto 0rem auto 1rem;
+        font-size: 1.5rem;
+    }
+`
+
+const NextButton = ({text, link, setSection}) => {
+    return (
+        <Wrapper>
+            <Link to={link} style={{margin: "1rem auto"}} onClick={() => setSection(link)}>
+            <ButtonWrapper>
+                <Text>{text} <FontAwesomeIcon icon={faChevronRight}/></Text>
+            </ButtonWrapper>
+        </Link>
+        </Wrapper>
+    )
+}
+
+export default NextButton;
